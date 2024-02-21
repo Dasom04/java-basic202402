f70218f90bb8df7c7c9612107eaeba7cbb0e7c0b
dasom <slz_952@naver.com>
2024-02-21 14:19:05 +0900

    # Java 기초 문법(조건문)

--- src/basic/constate/IfQuiz.java
+++ src/basic/constate/IfQuiz.java
@@ -25,34 +25,35 @@ public class IfQuiz {
         int num3 = sc.nextInt();
 
         int max, mid, min;
-//
-//        if (num1 > num2 && num1 > num3) {  // num1이 max다!!
-//            max = num1;
-//            if (num2 > num3) {
-//                mid = num2;
-//
-//            } else {
-//                mid = num3;
-//                min = num2;
-//            } else if (num2 > num1 && num2 > num3)  // num2가 max다!!
-//                max = num2;
-//            if (num1 > num3) {
-//                mid = num1;
-//                min = num3;
-//            } else {
-//                mid = num3;
-//                min = num1;
-//            }
-//        } else {    // num3이 max다!!
-//            max = num3;
-//            if (num1 > num2) {
-//                mid = num1;
-//                min = num2;
-//            } else {
-//                mid = num2;
-//                min = num1;
-//            }
-//        }
+
+        if(num1 > num2 && num1 > num3) { // num1이 max다!
+            max = num1;
+            if(num2 > num3) {
+                mid = num2; min = num3;
+            } else {
+                mid = num3; min = num2;
+            }
+        } else if(num2 > num1 && num2 > num3) { // num2가 max다!
+            max = num2;
+            if(num1 > num3) {
+                mid = num1; min = num3;
+            } else {
+                mid = num3; min = num1;
+            }
+        } else { // num3가 max다!
+            max = num3;
+            if(num1 > num2) {
+                mid = num1; min = num2;
+            } else {
+                mid = num2; min = num1;
+            }
+        }
+
+        System.out.println("==========================================");
+        System.out.println("max = " + max);
+        System.out.println("mid = " + mid);
+        System.out.println("min = " + min);
+
         sc.close();
     }
 
