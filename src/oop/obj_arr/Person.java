5bf3c5fc151d43ceb034fa6c69d22b2b4f93b20d
dasom <slz_952@naver.com>
2024-02-26 18:05:09 +0900

    # Java oop(오버로딩, 객체배열)

---
+++ src/oop/obj_arr/Person.java
@@ -0,0 +1,77 @@
+package oop.obj_arr;
+
+
+public class Person {
+
+    private String name;
+    private  int age;
+    private  String gender;
+
+    public Person() {}
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public void personInfo() {
+        System.out.printf("이름: %s\n 나이: %d\n 성별: %s\n"
+                          , this.name, this.age, this,gender);
+        System.out.println("--------------------------------");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
