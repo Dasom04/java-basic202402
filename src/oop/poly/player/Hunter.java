aa01fc0f09f9b0fca8f37adc5bbae1c1bd9c28f1
dasom <slz_952@naver.com>
2024-02-27 16:57:45 +0900

    # Java oop(다형성 캐스팅, instanceof)

---
+++ src/oop/poly/player/Hunter.java
@@ -0,0 +1,16 @@
+package oop.poly.player;
+
+public class Hunter extends player {
+     String pet;
+
+     Hunter(String name) {
+         super(name);
+         this.pet = "멍멍이";
+     }
+
+    @Override
+    void characterInfo() {
+        super.characterInfo();
+        System.out.println("# 펫 이름: " + pet);
+    }
+}
