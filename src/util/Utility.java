2086b695591ba0ea4f875ccbe2cdcdce9a918043
dasom <slz_952@naver.com>
2024-03-06 15:16:34 +0900

    # Java oop(static 완료)

--- src/util/Utility.java
+++ src/util/Utility.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 
 public class Utility {
 
+    static Scanner sc = new Scanner(System.in);
+
     // 입력창의 구분을 위해 줄을 그어주는 메서드
     public static void makeLine() {
-
-       // static Scanner sc = new Scanner(System.in);
-
-
-
         System.out.println("===========================");
+    }
+
 
         // 문자열을 입력받는 기능
-      //  Public static String input(String message) {
-      //      System.out.println(message);
-     //       return sc.next();
+        public static String input (String message){
+            System.out.println(message);
+            return sc.next();
         }
-    }
+
+}
 
 
 
